d00325d178d0dcf64bf56aaeb42fcf6998ecd6d1
Caioba24 <dipietroapg@gmail.com>
2022-03-30 20:14:57 -0300

    adicionando text fields e botão

--- app/src/main/java/br/com/etecia/aulaandroid/LoginActivity.java
+++ app/src/main/java/br/com/etecia/aulaandroid/LoginActivity.java
@@ -3,6 +3,7 @@ package br.com.etecia.aulaandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+    }
+
+    public void acessarSistema(View view) {
+
     }
 }
